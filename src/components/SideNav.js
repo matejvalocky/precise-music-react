0bad2b7e2fafa229b73a74f52ea1aa40af9b31d7
Matej Valocky <matej.valocky@gmail.com>
2024-05-14 02:39:07 +0200

    dalšie úpravy ikon a textu

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -4,17 +4,22 @@ const SideNav = () => {
   return (
     <div className="sideNavigation">
       <div className="logoAndNav">
-        <div className="logoImage"></div>
+        <div className="logoImage">
+        <div className="opacityBlock">
+            <h2>Drum and Bass Mixes</h2>
+        </div>
+            
+        </div>
         <nav>
           <ul>
             <li>
               <div className="homeIcon icon"></div> Home
             </li>
             <li>
-              <div className="homeIcon icon"></div> About me
+              <div className="aboutIcon icon"></div> About me
             </li>
             <li>
-              <div className="homeIcon icon"></div> Mixes
+              <div className="musicIcon icon"></div> Mixes
             </li>
             <li>
               <div className="homeIcon icon"></div>Links
@@ -30,6 +35,7 @@ const SideNav = () => {
         </div>
         <div className="year">
           <p>2024</p>
+          <a href="https://preview.themeforest.net/item/watson-vcard-resume-cv-portfolio-template/full_screen_preview/22526409" target="_blank">Template</a>
         </div>
       </div>
     </div>
