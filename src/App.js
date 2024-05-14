b50f494211cc70b07c918d23fd1b55fccb1afc76
Matej Valocky <matej.valocky@gmail.com>
2024-05-14 14:03:37 +0200

    pridanie komponenty MainContent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import SideNav from "./components/SideNav"
+import MainContent from "./components/MainContent"
 
 
 
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
      <SideNav></SideNav>
+     <MainContent></MainContent>
     </div>
   )
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import "./MainContent.css";
+
+const MainContent = () => {
+  return (
+    <div className="mainContent">
+      
+        <h1>Precise Music</h1>
+      
+    </div>
+  );
+};
+
+export default MainContent;
