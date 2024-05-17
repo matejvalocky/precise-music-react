3c46969dc5e690637e3917686dcd9248a4f76ff6
Matej Valocky <matej.valocky@gmail.com>
2024-05-18 00:26:35 +0200

    zfunkčnenie Links podstránky

---
+++ src/components/LinksContent.js
@@ -0,0 +1,13 @@
+import "./LinksContent.css"
+
+import React from 'react'
+
+const LinksContent = () => {
+  return <div className="linksContent content">
+    <div className="wrapper">
+        
+    </div>
+  </div>
+}
+
+export default LinksContent
--- src/pages/Links-page.js
+++ src/pages/Links-page.js
@@ -1,13 +1,20 @@
 import SideNav from "../components/SideNav"
+import LinksContent from "../components/LinksContent"
+import Hamburger from "../Hamburger"
 
 
 const Links = () => {
-  return (
-    <div className="app">
-     <SideNav></SideNav>
-     
-    </div>
-  )
+  return <div className="app">
+
+  <div className="hamburger-top">
+    <Hamburger />
+  </div>
+
+  <div className="siteContent">
+    <SideNav></SideNav>
+    <LinksContent></LinksContent>
+  </div>
+</div>
 }
 
 export default Links
