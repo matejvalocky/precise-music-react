a8b34fa8f4893a6cea2f84bc075366b1705baf91
Matej Valocky <matej.valocky@gmail.com>
2024-09-27 00:40:52 +0200

    po klinutí na hamburger menu, toto menu zmizne

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,45 +1,50 @@
-import "./Hamburger.css"
-import "./components/HamburgerQueries.css"
-
-import { Link } from "react-router-dom"
-
-
-
-
-
-
+import "./Hamburger.css";
+import "./components/HamburgerQueries.css";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 const Hamburger = () => {
+  const checkboxRef = useRef(null);
+
+  const handleClick = () => {
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = false;
+    }
+  };
+
   return (
     <div className="hamburgerMenu">
       <nav>
-      <div className="navbar">
-        <div className="container nav-container">
-            <input className="checkbox" type="checkbox" name="" id="" />
+        <div className="navbar">
+          <div className="container nav-container">
+            <input 
+              className="checkbox" 
+              type="checkbox" 
+              name="" 
+              id="" 
+              ref={checkboxRef} 
+            />
             
-              <div className="hamburger-lines">
-                <span className="line line1"></span>
-                <span className="line line2"></span>
-                <span className="line line3"></span>
-              </div>  
+            <div className="hamburger-lines">
+              <span className="line line1"></span>
+              <span className="line line2"></span>
+              <span className="line line3"></span>
+            </div>  
 
-              <h1>Precise Music Official Website</h1>
-              <div className="logoImage logoNav"></div>
+            <h1>Precise Music Official Website</h1>
+            <div className="logoImage logoNav"></div>
 
-            
-           
-          <div className="menu-items">
-            <li> <Link to="/" className="menuLink menuLinkHamburger"><div className="homeIcon icon hamburgerIcon"></div><p>Home</p></Link></li>
-            <li> <Link to="/about" className="menuLink menuLinkHamburger"><div className="aboutIcon icon hamburgerIcon"></div><p>About</p></Link></li>
-            <li> <Link to="/mixes" className="menuLink menuLinkHamburger"><div className="musicIcon icon hamburgerIcon"></div><p>Mixes</p> </Link></li>
-            <li> <Link to="/links" className="menuLink menuLinkHamburger"><div className="linksIconNav icon hamburgerIcon"></div> <p>Links</p></Link></li>
+            <div className="menu-items">
+              <li><Link to="/" className="menuLink menuLinkHamburger" onClick={handleClick}><div className="homeIcon icon hamburgerIcon"></div><p>Home</p></Link></li>
+              <li><Link to="/about" className="menuLink menuLinkHamburger" onClick={handleClick}><div className="aboutIcon icon hamburgerIcon"></div><p>About</p></Link></li>
+              <li><Link to="/mixes" className="menuLink menuLinkHamburger" onClick={handleClick}><div className="musicIcon icon hamburgerIcon"></div><p>Mixes</p></Link></li>
+              <li><Link to="/links" className="menuLink menuLinkHamburger" onClick={handleClick}><div className="linksIconNav icon hamburgerIcon"></div><p>Links</p></Link></li>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Hamburger
+export default Hamburger;
