325036306580349f4efb4e60238561c58bac7169
Matej Valocky <matej.valocky@gmail.com>
2024-09-27 00:17:27 +0200

    odstranenie animacie z homepage

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,34 +3,17 @@ import "./Content.css";
 import Slideshows from "../components/Slideshows";
 
 // REACT SPRING - ANIMATION
-import { useSpring, animated } from '@react-spring/web';
-
 
 // KONKRETNE CSS
 import "./MainContent.css";
 const MainContent = () => {
-
-  const props = useSpring({
-    opacity: 1,
-    transform: 'translateY(0px)',
-    
-    from: {
-      opacity: 0,
-      transform: 'translateY(20px)',
-      
-    },
-    config: { tension: 170, friction: 26 },
-  });
-
-  return <animated.div style={props}>
-        <div className="fullFrame">
+  return (
+    <div className="fullFrame">
       <div className="mainHeadingSlideshow">
-        <Slideshows/>
+        <Slideshows />
       </div>
     </div>
-  </animated.div>
-
-  
+  );
 };
 
 export default MainContent;
